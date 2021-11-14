8e2ecee063435f9177af99f8f69fdbe260d6ddd4
Nafie <warrior259@gmail.com>
2021-11-14 20:46:05 +0300

    add user date_created

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+import datetime
 import sqlalchemy as sql
 import passlib.hash as hash
 import db
@@ -12,6 +13,7 @@ class User(db.Base):
     name = sql.Column(sql.String)
     email = sql.Column(sql.String, unique=True)
     hashed_password = sql.Column(sql.String)
+    date_created = sql.Column(sql.DateTime, default=datetime.datetime.utcnow())
 
     def verify_password(self, password: str) -> bool:
         return hash.bcrypt.verify(password, self.hashed_password)
